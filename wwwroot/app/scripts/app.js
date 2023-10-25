e0c585c157bea84bbc5892e44cb9fc8a5cff95b7
dharmesh-b <55308280+dharmesh-b@users.noreply.github.com>
2023-10-25 19:46:01 +0530

    Update app.js

--- wwwroot/app/scripts/app.js
+++ wwwroot/app/scripts/app.js
@@ -11,7 +11,7 @@ angular.module('todoApp', ['ngRoute'])
     $routeProvider.when("/Home", {
         controller: "todoListCtrl",
     //    templateUrl: "/App/Views/TodoList.html",
-        templateUrl: "http://localhost:5000/App/Views/TodoList.html",
+        templateUrl: "/App/Views/TodoList.html",
     }).otherwise({ redirectTo: "/Home" });
 
     }]);
